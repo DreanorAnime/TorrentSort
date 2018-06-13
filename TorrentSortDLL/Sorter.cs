0d75fe60d111144748683e6c687628ecf9923bfc
Mike <mikeundead@googlemail.com>
2018-06-13 20:07:41 +0200

    new algorithm

--- TorrentSortDLL/Sorter.cs
+++ TorrentSortDLL/Sorter.cs
@@ -25,6 +25,7 @@ namespace TorrentSortDLL
 
             Regex regexFilename = new Regex(config.FileRegex);
             Regex regexFolder = new Regex(config.FolderRegex);
+            Regex regexWhitespace = new Regex("[ ]{2,}");
 
             foreach (var file in Directory.GetFiles(config.RootFolder, config.FileFilter))
             {
@@ -35,8 +36,11 @@ namespace TorrentSortDLL
                     filename = filename.Replace(match.Value, string.Empty).Trim();
                 }
 
+                var folderNameWhitespaces = regexFolder.Replace(filename, string.Empty);
+                var folderNameFinal = regexWhitespace.Replace(folderNameWhitespaces, " ");
+
                 var ext = $"{filename}{Path.GetExtension(file)}";
-                var path = Path.Combine(config.RootFolder, regexFolder.Matches(filename)[0].Value.Trim());
+                var path = Path.Combine(config.RootFolder, folderNameFinal);
                 Directory.CreateDirectory(path);
                 File.Move(file, Path.Combine(path, ext));
             }
