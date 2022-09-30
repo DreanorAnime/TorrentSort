097c88b137a22740d2c512af1b1e91ff4ac2a317
Dreanor <mikeundead@googlemail.com>
2022-09-30 23:03:20 +0200

    some bugfixes

---
+++ TorrentSort/Config.cs
@@ -0,0 +1,13 @@
+﻿namespace TorrentSortDLL
+{
+    public class Config
+    {
+        public string RootFolder { get; set; }
+
+        public string FileRegex { get; set; }
+
+        public string FolderRegex { get; set; }
+
+        public string FileFilter { get; set; }
+    }
+}
